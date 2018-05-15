e6e7b041453186eb8188f016aff94bcbb974227d
0tiz <0tiz@users.noreply.github.com>
2018-05-15 16:34:23 +0200

    ShakerSort Finish

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -10,7 +10,7 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
-            int[] unsortArray = new int[5] { 12, 11, 10, 3, 7 };
+            int[] unsortArray = new int[5] { 12, 11, 15, 3, 7 };
             CocktailSort(unsortArray);
             Console.WriteLine(String.Join(", ", unsortArray));
         }
@@ -31,40 +31,57 @@ namespace InsertionSort
                     {
                         int cache = 0;
                         cache = array[i];
-                        array[i] = array[i -1];
+                        array[i] = array[i - 1];
                         array[i - 1] = cache;
                         didSwap = true;
                     }
 
-                   
+
 
                 }
 
 
             }
-           
+
         }
 
         private static void CocktailSort(int[] array)
         {
             bool didSwap = true;
-            while(didSwap == true)
+            int lowerBound = 1;
+            int upperBound = array.Length;
+            while (didSwap == true)
             {
-                for(int i = 1; i < array.Length; i++)
-                { //eine Richtung 
-                    if(array[i -1] > array[i])
+
+                didSwap = false;
+
+                for (int i = lowerBound; i < upperBound; i++)
+                {
+                    if (array[i - 1] > array[i])
                     {
                         int cache = 0;
                         cache = array[i];
                         array[i] = array[i - 1];
                         array[i - 1] = cache;
+                        didSwap = true;
                     }
-
-                    
-
                 }
-                didSwap = false;
+                upperBound = upperBound - 1;
+
+                for (int i = upperBound - 1; i > lowerBound - 1; i--)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        int cache = 0;
+                        cache = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = cache;
+                        didSwap = true;
+                    }
+                }
+                lowerBound = lowerBound + 1;
             }
+
         }
 
         private static void QuickSort(int[] array)
